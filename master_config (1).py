f04ebc4ac59c21142e50660690676ad72b80b97b
Shashank Hegde <32035347+Shashank-Hegde@users.noreply.github.com>
2025-04-28 08:50:26 +0200

    Update master_config (1).py

--- master_config (1).py
+++ master_config (1).py
@@ -44,7 +44,7 @@ symptom_synonyms = {
         'persistent noggin ache', 'head tenderness', 'scalp-aching feeling', 'sensitive head region', 'brainache', 'mind-throbbing torment', 'front-lobe pressure', 'crown-of-head tension',
         'behind-the-eyes ache', 'skull-tight discomfort', 'never-ending head throb', 'grating ache inside skull', 'sinus-pressured ache', 'temple pounding', 'brain pulsation pain', 'cephalic torment',
         'oppressive ache under cranium', 'subtle persistent ache', 'gnawing head discomfort', 'dull pounding drumbeat in head', 'hammering inside skull walls', 'unyielding head tension', 'rote ache cycling through head',
-        'cranium under siege', 'deep-set head pang', 'swirling headache sensation', 'anchor-like pressure in head','pain in head'
+        'cranium under siege', 'deep-set head pang', 'swirling headache sensation', 'anchor-like pressure in head','pain in head','head pounding like drums','issue with head'
     ],
     'migraine': [
         'intense one-sided headache', 'migraine aura', 'pulsating pain in head', 'photophobia-associated headache', 'debilitating headache', 'migraine attack', 'searing half-skull ache', 'throbbing temple migraine',
