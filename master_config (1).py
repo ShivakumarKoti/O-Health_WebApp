4a76a8902ec9de9813d9bb710b40c5bbdaf75dd0
Shashank Hegde <32035347+Shashank-Hegde@users.noreply.github.com>
2025-05-28 09:08:42 +0200

    Update master_config (1).py

--- master_config (1).py
+++ master_config (1).py
@@ -9748,7 +9748,7 @@ body_parts = [
     'fingertip', 'fingertips', 'instep', 'calf', 'shin','lumbar', 'thoracic', 'cervical', 'gastrointestinal', 'abdominal', 'rectal', 'genital',
     'urinary', 'respiratory', 'cardiac', 'pulmonary', 'digestive', 'cranial', 'facial', 'face',
     'ocular', 'otologic', 'nasal', 'oral', 'buccal', 'lingual', 'pharyngeal', 'laryngeal', 'heart',
-    'trigeminal', 'spinal', 'peripheral', 'visceral', 'biliary', 'renal', 'hepatic','period','jaw','hip','calf','body'
+    'trigeminal', 'spinal', 'peripheral', 'visceral', 'biliary', 'renal', 'hepatic','period','jaw','hip','calf','waist', 'pelvic','body'
 ] 
 
 symptom_to_specialist = {
