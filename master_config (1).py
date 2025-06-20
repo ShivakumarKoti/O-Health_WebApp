533bd62e09fad0077f57525d20b4aedb26638f83
Shashank Hegde <32035347+Shashank-Hegde@users.noreply.github.com>
2025-06-20 11:56:26 +0200

    Update master_config (1).py

--- master_config (1).py
+++ master_config (1).py
@@ -18,7 +18,7 @@ symptom_list = [
 'female issue', 'menopause', 'thyroid', 'piles', 'asthma','pneumonia','sugar',  'tingling', 'difficulty speaking',
 'brittle nails', 'more hungry', 'obesity', 'seizures', 'hiccups', 'ulcers', 'dysentery', 'malaria', 'dengue', 'covid','typhoid', 'chickenpox', 'kidney issue',
 'caesarean section','pregnancy', 'pediatric symptoms', 'blood in urine','broken voice', 'wound', 'cold intolerance', 'goiter','slow reflexes',
-'male reproductive issues', 'female reproductive issues', 'dandruff','blister','bruises','Not losing weight', '',
+'male reproductive issues', 'female reproductive issues', 'dandruff','blister','bruises','not losing weight', '',
 #  'arthritis'
 ]
 
@@ -6334,7 +6334,8 @@ trigger_keywords = {
 	'freeze': ['freeze', 'freezing', 'frozen', 'chilled'],
 	'spasm': ['spasm', 'spasms', 'cramp', 'tightness', 'twitching'],
 	'itching': ['itch', 'itching', 'scratchy', 'itchiness'],
-	'bleeding': ['bleed', 'bleeding', 'bleeds', 'bled', 'blood']
+	'bleeding': ['bleed', 'bleeding', 'bleeds', 'bled', 'blood'],
+	#'burn': ['burn','burning','burns']
     },
 'shoulder': {
         'pain': ['pain', 'pains', 'hurt', 'hurts', 'sore', 'sores', 'ache', 'aches', 'aching','throbbing',],
